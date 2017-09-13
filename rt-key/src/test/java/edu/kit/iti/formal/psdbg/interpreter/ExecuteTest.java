c3dba26d071447be70df4f370720f8fb2c2bdf13
Sarah Grebing <grebing@ira.uka.de>
2017-09-13 11:21:10 +0200

    Adding test cases

--- rt-key/src/test/java/edu/kit/iti/formal/psdbg/interpreter/ExecuteTest.java
+++ rt-key/src/test/java/edu/kit/iti/formal/psdbg/interpreter/ExecuteTest.java
@@ -1,13 +1,20 @@
 package edu.kit.iti.formal.psdbg.interpreter;
 
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.sort.Sort;
+import de.uka.ilkd.key.parser.DefaultTermParser;
+import de.uka.ilkd.key.parser.ParserException;
 import static edu.kit.iti.formal.psdbg.TestHelper.getFile;
 import edu.kit.iti.formal.psdbg.interpreter.data.KeyData;
 import edu.kit.iti.formal.psdbg.interpreter.data.State;
+import edu.kit.iti.formal.psdbg.parser.ast.Variable;
+import edu.kit.iti.formal.psdbg.parser.data.Value;
 import org.apache.commons.cli.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.StringReader;
 
 /**
  * @author Alexander Weigl
@@ -48,15 +55,25 @@ public class ExecuteTest {
     }
 
     @Test
-    public void testContrapositionCut() throws IOException, ParseException {
+    public void testContrapositionCut() throws IOException, ParseException, ParserException {
         Execute execute = create(
                 getFile(getClass(), "contraposition/contraposition.key"),
                 "-s", getFile(getClass(), "contraposition/cutTest.kps"));
         Interpreter<KeyData> i = execute.run();
         State<KeyData> currentState = i.getCurrentState();
         System.out.println(currentState);
-        //This reveals a bug in the cases merge
+        //This revealed a bug in the interpreter for body of a case
         Assert.assertEquals("Number of goals has to be two ", 2, i.getCurrentGoals().size());
+        KeyData goal0_data = currentState.getGoals().get(0).getData();
+        Value y = currentState.getGoals().get(0).getAssignments().getValue(new Variable("Y"));
+        Assert.assertTrue(goal0_data.getRuleLabel().contains("cut"));
+        KeyData goal1_data = currentState.getGoals().get(1).getData();
+        Assert.assertTrue(goal1_data.getRuleLabel().contains("cut"));
+        DefaultTermParser dp = new DefaultTermParser();
+
+        Term s = dp.parse(new StringReader(y.getData().toString()), Sort.FORMULA, goal0_data.getEnv().getServices(), goal0_data.getProof().getNamespaces(), null);
+        Assert.assertEquals("Antecedent should contain the cut formula", s, goal0_data.getNode().sequent().antecedent().get(0).formula());
+
 
     }
 }
