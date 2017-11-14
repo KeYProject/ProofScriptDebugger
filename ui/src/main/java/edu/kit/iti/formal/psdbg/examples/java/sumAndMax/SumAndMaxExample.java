e7ca8e3f7373f98ad92dcc9695ad2f67062e7b7c
Sarah Grebing <grebing@ira.uka.de>
2017-11-14 15:16:33 +0100

    added lulus example

---
+++ ui/src/main/java/edu/kit/iti/formal/psdbg/examples/java/sumAndMax/SumAndMaxExample.java
@@ -0,0 +1,17 @@
+package edu.kit.iti.formal.psdbg.examples.java.sumAndMax;
+
+import edu.kit.iti.formal.psdbg.examples.JavaExample;
+
+public class SumAndMaxExample extends JavaExample {
+
+    public SumAndMaxExample() {
+        setName("SumAndMax Example");
+        setJavaFile(this.getClass().getResource("SumAndMax.java"));
+
+
+        defaultInit(getClass());
+
+        System.out.println(this);
+
+    }
+}
